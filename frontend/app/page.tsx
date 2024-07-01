9b5d912ee220d3648f5c0a6c50baac0434a5ab00
real-Vansh-Khare <kharevansh13@gmail.com>
2024-07-01 11:36:43 +0530

    Add basic homepage design

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center text-center justify-center h-[40rem] gap-8 p-2">
+      <div className="text-5xl mb-8">Welcome to InkPPT</div>
+      <div className="text-3xl">Convert blueprints to presentations with InkPPT</div>
+      <button className="text-xl bg-green-600 p-2 text-white rounded-xl cursor-pointer">Generate PPT</button>
     </main>
   );
 }
